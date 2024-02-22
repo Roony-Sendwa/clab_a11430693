3884b92ac4617d267a30094b21b1342531eaf3ca
Roony Sendwa <roony.shisuvili@student.manchester.ac.uk>
2024-02-22 20:48:12 +0000

    task_uploada

---
+++ task_upload.c
@@ -0,0 +1,11 @@
+#include <stdio.h> // directive to include 
+
+
+int main(void)   // function run when the programs starts 
+
+{ 
+
+printf("Hello. My email address is: roony.shisuvili@student.manchester.ac.uk"); // command to run 
+ 
+    return 0; // the return statement for the function main
+}
